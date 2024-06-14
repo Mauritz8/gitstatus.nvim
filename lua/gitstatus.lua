1e053b1cfd94e2f99ecb97849bb48b27efc11f60
Mauritz <mauritz.sjodin3@gmail.com>
2024-06-14 13:09:05 +0200

    feat: make buffer not modifiable

--- lua/gitstatus.lua
+++ lua/gitstatus.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.open_status_win()
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, {'hello', 'goodbye'})
+  vim.api.nvim_set_option_value('modifiable', false, { buf = buf })
   vim.api.nvim_open_win(buf, true, {
     split = 'left',
     width = 50,

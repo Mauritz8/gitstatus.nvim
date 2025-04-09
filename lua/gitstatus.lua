ac130ac1fdfede1102cb55079831948072b3cb38
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-09 10:11:24 +0200

    feat: show message if no changes

--- lua/gitstatus.lua
+++ lua/gitstatus.lua
@@ -130,24 +130,37 @@ local function highlight_group(file_state)
   return file_state == FILE_STATE.staged and 'Added' or 'Removed'
 end
 
+---@param buf integer
+---@param i integer
+---@param file File
+local function set_line(buf, i, file)
+  local line_nr = i - 1
+  local line = prefix(file.type) .. file.name
+  vim.api.nvim_buf_set_lines(buf, line_nr, line_nr, true, {line})
+
+  local ns_id = vim.api.nvim_create_namespace("")
+  local hl_group = highlight_group(file.state)
+  vim.api.nvim_buf_set_extmark(buf, ns_id, line_nr, 0, {
+    end_col = string.len(line),
+    hl_group = hl_group,
+  })
+end
+
 function M.open_status_win()
   local buf = vim.api.nvim_create_buf(false, true)
+
   local files = retrieve_files()
-  for i, file in pairs(files) do
-    local line_nr = i - 1
-    local line = prefix(file.type) .. file.name
-    local hl_group = highlight_group(file.state)
-    vim.api.nvim_buf_set_lines(buf, line_nr, line_nr, true, {line})
-    local ns_id = vim.api.nvim_create_namespace("")
-    vim.api.nvim_buf_set_extmark(buf, ns_id, line_nr, 0, {
-      end_col = string.len(line),
-      hl_group = hl_group,
-    })
+  if #files == 0 then
+    local content = "nothing to commit, working tree clean"
+    vim.api.nvim_buf_set_lines(buf, 0, 0, true, {content})
+  else
+    for i, file in pairs(files) do
+      set_line(buf, i, file)
+    end
   end
 
+
   vim.api.nvim_set_option_value('modifiable', false, { buf = buf })
-  vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>',
-      '<CMD>lua require("gitstatus").open_file_current_line()<CR>', {})
   vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<CMD>:q<CR>', {})
 
   vim.api.nvim_open_win(buf, true, {

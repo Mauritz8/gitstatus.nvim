7f770a178e997a3120d8575b7d0e590c0e0e9b41
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-09 19:49:30 +0200

    feat: toggle stage file

--- lua/git_actions.lua
+++ lua/git_actions.lua
@@ -10,4 +10,17 @@ function M.stage_file(file)
   end
 end
 
+---@param file string
+function M.unstage_file(file)
+  local obj = vim.system(
+    {'git', 'restore', '--staged', file},
+    { text = true }
+  ):wait()
+  if obj.code ~= 0 then
+    vim.print("Unable to unstage file:", obj.stderr)
+  else
+    vim.print(("Successfully unstaged %s!"):format(file))
+  end
+end
+
 return M
--- lua/gitstatus.lua
+++ lua/gitstatus.lua
@@ -3,6 +3,15 @@ local git_actions = require("git_actions")
 
 local M = {}
 
+---@class Line
+---@field str string
+---@field highlight_group string
+---@field file File?
+Line = {}
+
+---@type Line[]
+local buf_lines = {}
+
 ---@param file_edit_type FILE_EDIT_TYPE
 ---@return string
 local function prefix(file_edit_type)
@@ -37,12 +46,6 @@ local function split_files_by_state(files)
   return staged, not_staged, untracked
 end
 
-
----@class Line
----@field str string
----@field highlight_group string
-Line = {}
-
 ---@param files File[]
 ---@return Line[]
 local function get_lines(files)
@@ -53,16 +56,18 @@ local function get_lines(files)
     table.insert(lines, {
       str = "Branch: " .. branch,
       highlight_group = nil,
+      file = nil,
     })
   end
 
   if #files == 0 then
     if #lines > 0 then
-      table.insert(lines, { str = ""; highlight_group = nil })
+      table.insert(lines, { str = "", highlight_group = nil, file = nil, })
     end
     table.insert(lines, {
       str = "nothing to commit, working tree clean",
-      highlight_group = nil
+      highlight_group = nil,
+      file = nil,
     })
     return lines
   end
@@ -75,14 +80,15 @@ local function get_lines(files)
   for i, files_of_type in ipairs(file_table) do
     if #files_of_type > 0 then
       if #lines > 0 then
-        table.insert(lines, { str = ""; highlight_group = nil })
+        table.insert(lines, { str = "", highlight_group = nil, file = nil, })
       end
-      table.insert(lines, { str = name(i); highlight_group = nil })
+      table.insert(lines, { str = name(i); highlight_group = nil, file = nil, })
     end
     for _, file in ipairs(files_of_type) do
       local line = {
         str = prefix(file.type) .. file.name,
         highlight_group = highlight_group(file.state),
+        file = file,
       }
       table.insert(lines, line)
     end
@@ -98,8 +104,8 @@ local function set_content(buf)
   vim.api.nvim_set_hl_ns(ns_id)
 
   local files = parser.retrieve_files()
-  local lines = get_lines(files)
-  for i, line in ipairs(lines) do
+  buf_lines = get_lines(files)
+  for i, line in ipairs(buf_lines) do
     local line_nr = i - 1
     vim.api.nvim_buf_set_lines(buf, line_nr, line_nr, true, {line.str})
     vim.api.nvim_buf_set_extmark(buf, ns_id, line_nr, 0, {
@@ -116,7 +122,7 @@ function M.open_status_win()
   vim.api.nvim_set_option_value('modifiable', false, { buf = buf })
   vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<CMD>q<CR>', {})
   vim.api.nvim_buf_set_keymap(buf, 'n', 's',
-      '<CMD>lua require("gitstatus").stage_file()<CR>', {})
+      '<CMD>lua require("gitstatus").toggle_stage_file()<CR>', {})
 
   vim.api.nvim_open_win(buf, true, {
     relative = 'editor',
@@ -127,45 +133,22 @@ function M.open_status_win()
   })
 end
 
---- TODO: make staging branch name line invalid
----@param line string
----@return string?
-local function file_on_line(line)
-  local invalid_lines = {
-    "",
-    "Staged:",
-    "Not staged:",
-    "Untracked:",
-  }
-  for _, invalid_line in ipairs(invalid_lines) do
-    if line == invalid_line then
-      return nil
-    end
-  end
-
-  local i = line:find(":")
-  if i == nil then
-    return line
-  end
-  local file_start = i + 2
-  if file_start >= line:len() then
-    return nil
-  end
-  return line:sub(file_start)
-end
-
-function M.stage_file()
-  local line = vim.api.nvim_get_current_line()
-  local file = file_on_line(line)
-  if file == nil then
+function M.toggle_stage_file()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local line = buf_lines[row]
+  if line.file == nil then
     vim.print("Unable to stage file: invalid line")
-  else
-    git_actions.stage_file(file)
-    local buf = vim.api.nvim_get_current_buf()
-    vim.api.nvim_set_option_value('modifiable', true, { buf = buf })
-    vim.api.nvim_buf_set_lines(buf, 0, -1, true, {})
-    set_content(buf)
+    return;
   end
+  if line.file.state == FILE_STATE.staged then
+    git_actions.unstage_file(line.file.name)
+  else
+    git_actions.stage_file(line.file.name)
+  end
+  local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_set_option_value('modifiable', true, { buf = buf })
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, {})
+  set_content(buf)
 end
 
 return M

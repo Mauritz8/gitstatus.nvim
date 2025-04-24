ee4b9aff1a1f2d4b14c389e30359b6c56e8da824
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-24 19:53:36 +0200

    fix: parse renamed path with `-` or `>` correctly
    close #46

--- lua/gitstatus/parse.lua
+++ lua/gitstatus/parse.lua
@@ -6,11 +6,26 @@ local M = {}
 ---@param delim string
 ---@return string[]
 local function split(str, delim)
-  local lines = {}
-  for line in str:gmatch('[^' .. delim .. ']+') do
-    table.insert(lines, line)
+  if #str == 0 then
+    return {}
   end
-  return lines
+
+  local matches = {}
+  local match_start = 1
+  while true do
+    local delim_start, delim_end = str:find(delim, match_start)
+    if delim_start ~= nil and delim_end ~= nil then
+      table.insert(matches, str:sub(match_start, delim_start - 1))
+      match_start = delim_end + 1
+    else
+      local remaining_str = str:sub(match_start)
+      if #remaining_str > 0 then
+        table.insert(matches, remaining_str)
+      end
+      break
+    end
+  end
+  return matches
 end
 
 ---@param str string
